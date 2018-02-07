12257893ec4a787a35c94955ffc7e5f7936511ad
Luís Mendes <luis.alvela@gmail.com>
2018-02-07 18:00:25 +0000

    Finished generator first exercise from slides.

--- Totoloto/src/generator/randomPrinter.java
+++ Totoloto/src/generator/randomPrinter.java
@@ -1,9 +1,30 @@
 package generator;
 
-//random "class"
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class randomPrinter {
+	
+	public static boolean checkbets(int[][] matrix, int N) {
+		
+		int temp[] = new int[6];
+		
+		for(int a = 0; a<N; a++) {
+			
+			temp = matrix[a].clone();
+			
+			for(int j = 0; j < N; j++) {
+				if(temp.equals(matrix[j]) && a != j) {
+					//found two bets that are the same!
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
 	
 	public static boolean checkunique(int[] key) {
 		int temp;
@@ -29,10 +50,40 @@ public class randomPrinter {
 		
 		Random randomnumber = new Random(); //object of type Random
 		int rand;
-		int[] a = new int[6];	
+		int[] a = new int[6];
+		int j = 0;
+		
+		// Getting the value of N for step 4
+		
+		int N;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		try {
+		
+		System.out.print("Valor de N: ");
+		
+		N = keyboard.nextInt();
+		
+		}
+		
+		catch(InputMismatchException exception) {
+			System.out.print("The value you typed in for N is not an integer.");
+			return;
+		}
+		
+		if(N < 0) {
+			System.out.print("The value you typed in for N is negative, it must be positive.");
+			return;
+		}
+		
+		int[][] matrix = new int[N][6];
 		
 		System.out.println("Não pode repetir: ");
- 		
+
+while(true) {
+	
+	for(int z = 0; z < N; z++) {
 	while(true) {
 		
 		for(int i = 0; i<=5; i++) {
@@ -47,12 +98,35 @@ public class randomPrinter {
 		}
 		
 		if(checkunique(a)) {
+			
+			//if it's valid key, add to matrix
+			
+			//each iteration changes a column of the matrix (adds a key) up to the value of N.
+			//the matrix initially starts with N keys all set to 000000.
+			
+			//before adding to the matrix, sort it out.
+			Arrays.sort(a);
+			
+			for(int k = 0; k < 6; k++) {
+				matrix[z][k] = a[k];
+			}
+			
 			break; //if it returns true, it's a valid key. If it returns false, generate another one, cycle loops back.
 		}
 	}
-	
-	for(int i = 0; i<a.length; i++) {
-		System.out.println(a[i]);
+}
+	if(checkbets(matrix, N)) {
+		//if it returns true, valid matrix.
+		break;
+	}
+}
+		
+	for(int i = 0; i<N; i++) {
+		for(int v = 0; v<6; v++) {
+			System.out.println(matrix[i][v]);
+		}
+		
+		System.out.println(" ");
 	}
 	
 	}
